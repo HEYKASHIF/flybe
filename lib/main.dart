cccee974cb61e43136b8fff4cf68447b12e4153d
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-26 01:52:32 +0530

    26-march commit17

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
           actions: const [Icon(Icons.notification_important)],
         ),
         body: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Container(
               margin: EdgeInsets.all(11),
